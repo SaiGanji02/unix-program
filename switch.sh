a0ea1f5e6334cb7c60f40951a4635edd76fd905f
SaiGanji02 <94685883+SaiGanji02@users.noreply.github.com>
2021-11-23 11:08:36 +0530

    Create switch.sh

---
+++ switch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+word="saikumar"
+case= "$word" in "ganji";;
+        echo "word in ganji";;
+"sai")
+        echo "word in sai";;
+"saikumar")
+        echo "word in saikumar";;
+        esac
